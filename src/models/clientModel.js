f780d370f6f78420a90b45956b9d8ab7b616c318
rilton bispo <riltondev@gmail.com>
2023-11-23 14:55:49 -0300

    :recycle: Usando const query no client model

--- src/models/clientModel.js
+++ src/models/clientModel.js
@@ -5,7 +5,8 @@ const client = mysql.createPool(process.env.CONNECTION_STRING)
 
 const getClients = async () => {
   try {
-    const result = await client.query("SELECT * FROM Client;")
+    const query = "SELECT * FROM Client;"
+    const result = await client.query(query)
     return result[0]
   } catch (err) {
     console.log(`BANCO: Erro ao buscar os clientes: ${err}`)
@@ -14,7 +15,8 @@ const getClients = async () => {
 
 const getOneClient = async (id) => {
   try {
-    const result = await client.query("SELECT * FROM Client WHERE id=? ;", [id])
+    const query = "SELECT * FROM Client WHERE id=?;"
+    const result = await client.query(query, [id])
     return result[0]
   } catch (err) {
     console.log(`BANCO: Erro ao buscar o cliente: ${id}`)
@@ -23,8 +25,9 @@ const getOneClient = async (id) => {
 
 const insertClient = async (newClient) => {
   try {
+    const query = "INSERT INTO Client(name) VALUES (?);"
     const values = [newClient.name]
-    await client.query('INSERT INTO Client(name) VALUES (?);', values)
+    await client.query(query, values)
     console.log(`BANCO: Cliente ${newClient.name} criado com sucesso.`)
   } catch (err) {
     console.error(`BANCO: Erro ao criar o cliente ${newClient.name}: ${err.message}`)
@@ -33,9 +36,10 @@ const insertClient = async (newClient) => {
 
 const updateClient = async (id, clientData) => {
   try {
+    const query = "UPDATE Client SET name=? WHERE ID=?;"
     const values = [clientData.name, id]
-    await client.query('UPDATE Client SET name=? WHERE ID=?;', values)
-    console.log(`BANCO: Cliente ${clientData.id} atualizado com sucesso.`)
+    await client.query(query, values)
+    console.log(`BANCO: Cliente ${id} atualizado com sucesso.`)
   } catch (err) {
     console.error(`BANCO: Erro ao atualizar o cliente ${clientData.name}: ${err.message}`)
   }
@@ -43,8 +47,9 @@ const updateClient = async (id, clientData) => {
 
 const deleteClient = async (id) => {
   try {
+    const query = "DELETE FROM Client WHERE ID=?;"
     const values = [id]
-    await client.query('DELETE FROM Client WHERE ID=?;', values)
+    await client.query(query, values)
     console.log(`BANCO: Cliente ${id} deletado com sucesso.`)
   } catch (err) {
     console.error(`BANCO: Erro ao deletar o cliente ${id}: ${err.message}`)
